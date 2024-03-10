519fed656c6f9ba19ffbc2feb72ec1ee3b30eccb
idletim3 <128943623+idletim3@users.noreply.github.com>
2024-03-10 23:33:07 +0100

    Update network-enumeration.sh
    Added "-Pn" to account for the windows hosts in the domain.

--- network-enumeration.sh
+++ network-enumeration.sh
@@ -3,5 +3,5 @@
 read -p "Enter the path to the hosts file: " hostsFile
 
 while read line; do
-    proxychains nmap -p- -sC -sV -A --min-rate 2500 "$line" -oN "nmap_$line"
+    proxychains nmap -p- -Pn -sC -sV -A --min-rate 2500 "$line" -oN "nmap_$line"
 done < "$hostsFile"
